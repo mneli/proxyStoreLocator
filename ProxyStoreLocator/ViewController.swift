6e3319a556a9b99c4490255f38220fb09dc1427f
mneli <m.nyeem@outlook.com>
2017-10-16 03:56:02 +0200

    add location permission & user location

--- ProxyStoreLocator/ViewController.swift
+++ ProxyStoreLocator/ViewController.swift
@@ -7,17 +7,48 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+	let locationManager = CLLocationManager()
+	let defaultLocation = CLLocationCoordinate2DMake(CLLocationDegrees(50.847974), CLLocationDegrees(4.350370))
+	let zoomDistance: CLLocationDistance = 700
+	
+	
+	@IBOutlet weak var mapView: MKMapView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		locationManager.delegate = self
+		locationManager.requestWhenInUseAuthorization()
+		locationManager.startUpdatingLocation()
+		zoomToUserLocation()
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+		guard status == .authorizedWhenInUse else {
+			let alert = UIAlertController(title: "Location permission not granted", message: "Please allow this app to get your location when in use for a better user experience under \nSettings > Privacy > Location Services", preferredStyle: .alert)
+			alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+			self.present(alert, animated: true, completion: nil)
+			return
+		}
+		mapView.showsUserLocation = true
+	}
+	
+	func zoomToUserLocation() {
+		let viewRegion: MKCoordinateRegion
+		if let userCurrentLoc = locationManager.location?.coordinate {
+			viewRegion = MKCoordinateRegionMakeWithDistance(userCurrentLoc, zoomDistance, zoomDistance)
+		} else {
+			viewRegion = MKCoordinateRegionMakeWithDistance(defaultLocation, zoomDistance, zoomDistance)
+		}
+		mapView.setRegion(viewRegion, animated: true)
 	}
 
 
